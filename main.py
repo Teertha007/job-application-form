eaad3594e02f766973e2925cd60c22376ac686a5
Teertha007 <teertha.sarker.3@gmail.com>
2025-09-22 03:05:55 +0600

    refactor main.py to organize Flask app structure and enhance form handling

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config["Secret_Key"] = "myapplication123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         # Handle form submission
         first_name = request.form.get('first_name')
-        print(first_name)
         last_name = request.form.get('last_name')
         email = request.form.get('email')
         date = request.form.get('date')
+        date_obj= datetime.strptime(date, '%Y-%m-%d')
         occupation = request.form.get('occupation')
         # Process the data as needed
+
+        form = Form(first_name=first_name, 
+                    last_name=last_name,
+                    email=email, 
+                    date=date_obj,
+                    occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+
     return render_template('index.html')
 
-app.run(debug=True,port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True,port=5000)

b0369604816e599c524078a3e4995e4f8652ade5
Teertha007 <teertha.sarker.3@gmail.com>
2025-09-22 02:39:46 +0600

    enhance job application form with Bootstrap styling and handle form submission in Flask

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        # Handle form submission
+        first_name = request.form.get('first_name')
+        print(first_name)
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        date = request.form.get('date')
+        occupation = request.form.get('occupation')
+        # Process the data as needed
     return render_template('index.html')
 
 app.run(debug=True,port=5000)
